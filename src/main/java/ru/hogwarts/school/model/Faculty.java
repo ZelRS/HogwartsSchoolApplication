de1fac0acb894ecff3675da3ec9f3c19f2f8ccb0
Roman Zelenin (ZelRS) <ZeleninRS1991@gmail.com>
2023-09-18 17:50:09 +0600

    добавлена библиотека Lombok

--- src/main/java/ru/hogwarts/school/model/Faculty.java
+++ src/main/java/ru/hogwarts/school/model/Faculty.java
@@ -2,15 +2,18 @@ package ru.hogwarts.school.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.util.Collection;
-import java.util.Objects;
 
 import static org.apache.commons.lang3.StringUtils.capitalize;
 
-@Entity
+@Entity(name = "Факультет")
 @JsonIgnoreProperties(value = {"students"})
+@Data
+@NoArgsConstructor
 public class Faculty {
     @Id
     @SequenceGenerator(name = "facultyIdSeq", sequenceName = "faculty_id_seq", allocationSize = 1)
@@ -25,66 +28,9 @@ public class Faculty {
     @JsonManagedReference
     private Collection<Student> students;
 
-    public Faculty() {
-    }
-
     public Faculty(long id, String name, String color) {
         this.id = id;
         this.name = capitalize(name.toLowerCase());
         this.color = color.toUpperCase();
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public Collection<Student> getStudents() {
-        return students;
-    }
-
-    public void setStudents(Collection<Student> students) {
-        this.students = students;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Faculty faculty = (Faculty) o;
-        return id == faculty.id && Objects.equals(name, faculty.name) && Objects.equals(color, faculty.color);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, color);
-    }
-
-    @Override
-    public String toString() {
-        return "Faculty{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", color='" + color + '\'' +
-                '}';
     }
 }
--- src/main/java/ru/hogwarts/school/model/Student.java
+++ src/main/java/ru/hogwarts/school/model/Student.java
@@ -1,13 +1,16 @@
 package ru.hogwarts.school.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.util.Objects;
 
 import static org.apache.commons.lang3.StringUtils.capitalize;
 
-@Entity
+@Entity(name = "Студент")
+@Data
+@NoArgsConstructor
 public class Student {
     @Id
     @SequenceGenerator(name = "studentIdSeq", sequenceName = "student_id_seq", allocationSize = 1)
@@ -23,66 +26,9 @@ public class Student {
     @JsonBackReference
     private Faculty faculty;
 
-    public Student() {
-    }
-
     public Student(long id, String name, int age) {
         this.id = id;
         this.name = capitalize(name.toLowerCase());
         this.age = age;
-    }
-
-    public Faculty getFaculty() {
-        return faculty;
-    }
-
-    public void setFaculty(Faculty faculty) {
-        this.faculty = faculty;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Student student = (Student) o;
-        return id == student.id && age == student.age && Objects.equals(name, student.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, age);
-    }
-
-    @Override
-    public String toString() {
-        return "Student{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", age=" + age +
-                '}';
     }
 }
