66f36e2ac573e68e8ad8ba823816865a185d0759
Roman Zelenin (ZelRS) <ZeleninRS1991@gmail.com>
2023-09-29 15:35:48 +0600

    коррекция тестов TestRestTemplate. подключение H2 базы данных.

--- src/main/java/ru/hogwarts/school/model/Student.java
+++ src/main/java/ru/hogwarts/school/model/Student.java
@@ -32,6 +32,12 @@ public class Student {
     @JsonBackReference
     private Faculty faculty;
 
+    public Student(Long id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
     public Student(Long id, String name, int age, Faculty faculty) {
         this.id = id;
         this.name = capitalize(name.toLowerCase());
