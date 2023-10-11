c155bbcb14d97ae7ca310e884e625701c7acbbee
Roman Zelenin (ZelRS) <ZeleninRS1991@gmail.com>
2023-10-11 18:16:46 +0600

    в методы сервисов добавлено логирование

--- src/main/java/ru/hogwarts/school/service/impl/AvatarServiceImpl.java
+++ src/main/java/ru/hogwarts/school/service/impl/AvatarServiceImpl.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.service.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -31,6 +33,8 @@ public class AvatarServiceImpl implements AvatarService {
     private final AvatarRepository avatarRepository;
     private final StudentService studentService;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(AvatarServiceImpl.class);
+
     public AvatarServiceImpl(AvatarRepository avatarRepository, StudentService studentService) {
         this.avatarRepository = avatarRepository;
         this.studentService = studentService;
@@ -38,6 +42,7 @@ public class AvatarServiceImpl implements AvatarService {
 
     @Override
     public void uploadAvatar(Long studentId, MultipartFile avatarFile) throws IOException {
+        LOGGER.info("Was invoked method for upload avatar to student with ID = {}", studentId);
         Student student = studentService.getById(studentId);
 
         Path filePath = Path.of(avatarsDir, student.getName() + "." + getExtensions(avatarFile.getOriginalFilename()));
@@ -60,19 +65,26 @@ public class AvatarServiceImpl implements AvatarService {
         avatar.setMediaType(avatarFile.getContentType());
         avatar.setData(generatorDataForDB(filePath));
         avatarRepository.save(avatar);
+        LOGGER.debug("The avatar was uploaded successfully");
     }
 
     @Override
     public Avatar findAvatar(Long id) {
+        LOGGER.info("Was invoked method for find avatar with ID = {}", id);
+        LOGGER.debug("The avatar was received successfully");
         return avatarRepository.findByStudentId(id).orElse(new Avatar());
     }
 
     @Override
     public Collection<Avatar> getAll(Integer pageNumber, Integer pageSize) {
+        LOGGER.info("Was invoked method for find all avatars where page has number {} and has size {}",
+                pageNumber, pageSize);
+        LOGGER.debug("List of avatars was received successfully");
         return avatarRepository.findAll(PageRequest.of(pageNumber - 1, pageSize)).getContent();
     }
 
     private byte[] generatorDataForDB(Path filePath) throws IOException {
+        LOGGER.info("Was invoked method for generate avatar from DB");
         try (InputStream is = Files.newInputStream(filePath);
              BufferedInputStream bis = new BufferedInputStream(is, 1024);
              ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
@@ -85,6 +97,7 @@ public class AvatarServiceImpl implements AvatarService {
             graphics2D.dispose();
 
             ImageIO.write(preview, getExtensions(filePath.getFileName().toString()), baos);
+            LOGGER.debug("The avatar was generated successfully");
             return baos.toByteArray();
         }
     }
--- src/main/java/ru/hogwarts/school/service/impl/StudentServiceImpl.java
+++ src/main/java/ru/hogwarts/school/service/impl/StudentServiceImpl.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.service.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.exception.EntityNotFoundException;
 import ru.hogwarts.school.exception.NullNameFieldException;
@@ -18,80 +20,105 @@ import static org.apache.commons.lang3.StringUtils.isBlank;
 public class StudentServiceImpl implements StudentService {
     private final StudentRepository studentRepository;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(StudentServiceImpl.class);
+
     public StudentServiceImpl(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
     }
 
     @Override
     public Student create(Student student) {
+        LOGGER.info("Was invoked method for create student");
         nameValidator(student);
         ageValidator(student.getAge());
         referenceNameMaker(student);
+        LOGGER.debug("The student was created successfully");
         return studentRepository.save(student);
     }
 
     @Override
     public Student update(Student student) {
+        LOGGER.info("Was invoked method for update student");
         nameValidator(student);
         ageValidator(student.getAge());
         referenceNameMaker(student);
         Optional<Student> UpdatedStudent = studentRepository.findById(student.getId());
         if (UpdatedStudent.isEmpty()) {
+            LOGGER.error("There is not student with ID = {}", student.getId());
             throw new EntityNotFoundException("Студент не найден");
         }
+        LOGGER.debug("The student was updated successfully");
         return studentRepository.save(student);
     }
 
     @Override
     public void deleteById(Long id) {
+        LOGGER.info("Was invoked method for delete student with ID = {}", id);
         Optional<Student> student = studentRepository.findById(id);
         if (student.isEmpty()) {
+            LOGGER.error("There is not student with ID = {}", id);
             throw new EntityNotFoundException("Студент не найден");
         }
         studentRepository.deleteById(id);
+        LOGGER.debug("The student was removed successfully");
     }
 
     @Override
     public Student getById(Long id) {
+        LOGGER.info("Was invoked method for get student with ID = {}", id);
         Optional<Student> student = studentRepository.findById(id);
         if (student.isEmpty()) {
+            LOGGER.error("There is not student with ID = {}", id);
             throw new EntityNotFoundException("Студент не найден");
         }
+        LOGGER.debug("The student was received successfully");
         return student.get();
     }
 
     @Override
     public Collection<Student> getAll() {
+        LOGGER.info("Was invoked method for get all students");
+        LOGGER.debug("List of students was received successfully");
         return studentRepository.findAll();
     }
 
     @Override
     public Collection<Student> getAllByAgeBetween(int min, int max) {
+        LOGGER.info("Was invoked method for get all students with age between {} and {}", min, max);
         ageValidator(min);
         ageValidator(max);
         if (max < min) {
+            LOGGER.error("Min age can't be less than max age");
             throw new StudentAgeException("Задан некорректный диапазон");
         }
+        LOGGER.debug("List of students was received successfully");
         return studentRepository.findByAgeBetween(min, max);
     }
 
     @Override
     public Integer getCountOfStudents() {
+        LOGGER.info("Was invoked method for get count of students");
+        LOGGER.debug("Count of students was received successfully");
         return studentRepository.getCountOfStudents();
     }
 
     @Override
     public Integer getAverageAgeOfStudents() {
+        LOGGER.info("Was invoked method for get average age of students");
+        LOGGER.debug("Average age of students was received successfully");
         return studentRepository.getAverageAgeOfStudents();
     }
 
     @Override
     public Collection<Student> getFiveLastStudents() {
+        LOGGER.info("Was invoked method for get five last students");
+        LOGGER.debug("List of five last students was received successfully");
         return studentRepository.getFiveLastStudents();
     }
 
     private static void ageValidator(int age) {
         if (age <= 0 || age > 100) {
+            LOGGER.error("Age can't be less than {} and more than {}", 0, 100);
             throw new StudentAgeException("Некорректный возраст");
         }
     }
@@ -102,6 +129,7 @@ public class StudentServiceImpl implements StudentService {
 
     private static void nameValidator(Student student) {
         if (isBlank(student.getName())) {
+            LOGGER.error("Name can't be empty");
             throw new NullNameFieldException("Вы не задали имя");
         }
     }
